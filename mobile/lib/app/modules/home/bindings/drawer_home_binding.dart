bcdd6641061da6916dc6fc2b8a72b15ad509ce9f
m7md179 <mohammad.taha5@hotmail.com>
2024-11-30 23:28:09 +0300

    removing mobile from coded

--- mobile/lib/app/modules/home/bindings/drawer_home_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/drawer_controller.dart';
-
-class DrawerHomeBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<MyDrawerController>(() => MyDrawerController());
-  }
-}
--- mobile/lib/app/modules/home/bindings/google_map_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/google_map_controller.dart';
-
-class GoogleMapBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<MapController>(() => MapController());
-  }
-}
